76ae24c0ce8d1beefb5bd8c35a87be63a9764e3f
mikaelcordeiro <mikaelcordeiro01@gmail.com>
2020-12-31 02:08:10 -0300

    Incrementa exemplos com condicionais

--- OSSD_Linux_Git/bash_if.sh
+++ OSSD_Linux_Git/bash_if.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/bash
 
+echo "Insira dois números"
+
+read var1 var2
+
+if [[ "$var1" -ge "$var2" ]]; then
+
+	echo "Desde que o mundo é mundo, $var1 é maior que $var2"
+
+fi
+
 x="$PATH"
+
+if [[ -z "$x" ]]; then
+
+	echo "PATH null"
+
+else
+
+	echo "PATH not null"
+
+fi
 
 paths="${x/":"/" "}"
 
