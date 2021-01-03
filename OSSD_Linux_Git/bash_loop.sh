08f4ede6d4754369927890de381c2f419dcc93aa
mikaelcordeiro <mikaelcordeiro01@gmail.com>
2021-01-03 17:28:47 -0300

    loopings for, while e until explicados

--- OSSD_Linux_Git/bash_loop.sh
+++ OSSD_Linux_Git/bash_loop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+################ for loop ################
+
 x="$PATH"
 
 paths="$(echo $x | sed s/":"/" "/g)"
@@ -19,3 +21,53 @@ for file in $(find . -name "*.txt"); do
 	rm "$file"
 
 done
+
+################ while loop ################
+
+n=1; max=5; password=""
+
+while [[ "$n" -lt "$max" ]]; do
+  
+	echo -n "Digite a senha: "
+
+	read password
+
+	if [[ "$password" == "linux"  ]]; then
+
+		echo "Acertou com $n tentativas"
+
+	fi
+
+	exit 0
+  
+	n=$(( $n + 1 ))
+
+done
+
+echo "Você gastou todas as tentatvas"
+
+exit 1
+
+################ until loop ################
+
+n=1; max=5; password=""
+
+until [[ "$n" -ge "$max" ]]; do
+
+	echo -n "Digite a senha: "
+
+	read password
+
+	if [[ "$password" == "linux" ]]; then
+
+		echo "Acertou com $n tentativas"
+
+	exit 0
+	
+	fi
+
+done
+
+echo "Você esgotou as tentativas de novo"
+
+exit 1
